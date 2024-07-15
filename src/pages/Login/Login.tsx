6487d9b011ea955a2ceb9b6ea33dee69e804c949
Aleix Vidal <aleixvidal@C02JQ05RQ6LR.(none)>
2024-07-15 13:21:50 +0200

    feat: start simple validate form

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,16 +1,31 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import { Input } from "../../components/ui/input";
+import { Button } from "../../components/ui/button";
 
 export const Login: FC = () => {
+  const [user, setUser] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log(user, password);
+  };
   return (
-    <>
-      <Input name="user" id="user" type="text" placeholder="Username" />
-      <Input
-        name="password"
-        id="password"
-        type="password"
-        placeholder="Email"
-      />
-    </>
+    <div className="space-y-6">
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <Input
+          value={user}
+          onChange={(event) => setUser(event.target.value)}
+          type="text"
+          placeholder="Username"
+        />
+        <Input
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+          type="password"
+          placeholder="Email"
+        />
+        <Button type="submit">Login</Button>
+      </form>
+    </div>
   );
 };
