bab75b2ceb3ff00ccc246191513e1b339ad1b4c1
Aleix Vidal <aleixvidal@C02JQ05RQ6LR.(none)>
2024-07-16 18:33:14 +0200

    feat: add lazy to views

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,30 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { lazy } from "react";
 import { ThemeProvider } from "./components/theme-provider";
-import { Layout } from "./pages/Layout/Layout";
-import { Login } from "./pages/Login/Login";
-import { Main } from "./pages/Main/Main";
-import { Register } from "./pages/Register/Register";
+
+const Layout = lazy(() =>
+  import("./pages/Layout/Layout").then((module) => ({
+    default: module.Layout,
+  }))
+);
+
+const Login = lazy(() =>
+  import("./pages/Login/Login").then((module) => ({
+    default: module.Login,
+  }))
+);
+
+const Main = lazy(() =>
+  import("./pages/Main/Main").then((module) => ({
+    default: module.Main,
+  }))
+);
+
+const Register = lazy(() =>
+  import("./pages/Register/Register").then((module) => ({
+    default: module.Register,
+  }))
+);
 
 export const App = () => {
   return (
